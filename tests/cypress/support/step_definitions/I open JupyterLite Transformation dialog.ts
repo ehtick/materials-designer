3c3c94e73dde33bce07b89393805931f54261188
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-05 17:13:09 -0700

    update: add step definition for opening jls

---
+++ tests/cypress/support/step_definitions/I open JupyterLite Transformation dialog.ts
@@ -0,0 +1,10 @@
+import { When } from "@badeball/cypress-cucumber-preprocessor";
+
+import MaterialDesignerPage from "../widgets/MaterialDesignerPage";
+
+When("I open JupyterLite Transformation dialog", () => {
+    new MaterialDesignerPage().designerWidget.headerMenu.selectMenuItemByNameAndItemNumber(
+        "Advanced",
+        7,
+    );
+});
