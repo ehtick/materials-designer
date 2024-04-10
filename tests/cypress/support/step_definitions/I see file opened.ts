63b9d71ecd2475661dfe50cebdf8875b45170853
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-09 18:17:13 -0700

    update: use new functions for step def

--- tests/cypress/support/step_definitions/I see file opened.ts
+++ tests/cypress/support/step_definitions/I see file opened.ts
@@ -3,13 +3,7 @@ import { When } from "@badeball/cypress-cucumber-preprocessor";
 import MaterialDesignerPage from "../widgets/MaterialDesignerPage";
 
 When("I see file {string} opened", (filename: string) => {
-    cy.get("#jupyter-lite-iframe", { log: true, timeout: 20000 })
-        .should(($iframe) => {
-            expect($iframe.contents()).to.exist;
-        })
-        .then(($iframe) => {
-            const body = $iframe.contents().find("body");
-            return cy.wrap(body, { log: true });
-        });
-    // new MaterialDesignerPage().designerWidget.jupyterLiteSession.waitForVisible();
+    const { jupyterLiteSession } = new MaterialDesignerPage().designerWidget;
+    jupyterLiteSession.waitForVisible("md");
+    jupyterLiteSession.checkFileOpened(filename);
 });
