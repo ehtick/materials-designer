7c6b5dd007b5e686a019ae029e5e179501eebf53
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-10 15:33:49 -0700

    update: read the code from cell

--- tests/cypress/support/step_definitions/I see code in the cell is.ts
+++ tests/cypress/support/step_definitions/I see code in the cell is.ts
@@ -2,8 +2,14 @@ import { When } from "@badeball/cypress-cucumber-preprocessor";
 
 import MaterialDesignerPage from "../widgets/MaterialDesignerPage";
 
+function normalizeText(code: string) {
+    return code.replace(/\s/g, "");
+}
+
 When("I see code in the cell {string} is:", (index: string, code: string) => {
     new MaterialDesignerPage().designerWidget.jupyterLiteSession
         .getCodeFromCell(parseInt(index, 10))
-        .should("eq", code);
+        .then((cellCode) => {
+            expect(normalizeText(cellCode)).to.equal(normalizeText(code));
+        });
 });
