cacd4faba09bb9ae64c3264a396ca5b4d91815e2
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-09 18:32:52 -0700

    update: add command to get iframe body

--- tests/cypress/support/commands.ts
+++ tests/cypress/support/commands.ts
@@ -36,3 +36,27 @@
 //     }
 //   }
 // }
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            getIframeBody(iframeSelector: string): Chainable<Element>;
+        }
+    }
+}
+
+Cypress.Commands.add("getIframeBody", (iframeSelector: string) => {
+    cy.log("Getting iframe body for selector:", iframeSelector);
+
+    return cy
+        .get(iframeSelector, { log: true, timeout: 10000 })
+        .should(($iframe) => {
+            // Ensure the iframe's window object is available
+            expect($iframe.contents().find("body")).to.exist;
+        })
+        .then(($iframe) => {
+            // Access the contentWindow's document body
+            const body = $iframe.contents().find("body");
+            return cy.wrap(body);
+        });
+});
