c622cd0ca65622b74b8df801468b1141fd8dbabe
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-11 13:49:21 -0700

    update: add step for kernel status

---
+++ tests/cypress/support/step_definitions/I see kernel status is.ts
@@ -0,0 +1,17 @@
+import { When } from "@badeball/cypress-cucumber-preprocessor";
+
+import MaterialDesignerPage from "../widgets/MaterialDesignerPage";
+
+When("I see kernel status is {string}", (expectedStatus: string) => {
+    const { jupyterLiteSession } = new MaterialDesignerPage().designerWidget;
+
+    cy.until({
+        it: () => {
+            return jupyterLiteSession.getStatusText().then((status: string) => {
+                const baseStatus = status.split(" | ")[1];
+                return baseStatus === expectedStatus;
+            });
+        },
+        timeout: 100000,
+    });
+});
