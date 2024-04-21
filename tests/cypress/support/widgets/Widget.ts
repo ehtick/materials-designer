280ef2f4b334db6de85da9713e81d829b35af937
VsevolodX <79542055+VsevolodX@users.noreply.github.com>
2024-04-20 19:42:26 -0700

    update: add timouts enum to widget

--- tests/cypress/support/widgets/Widget.ts
+++ tests/cypress/support/widgets/Widget.ts
@@ -1,5 +1,7 @@
+import { TimeoutType } from "@mat3ra/tede/src/js/cypress/Browser";
 import BaseWidget from "@mat3ra/tede/src/js/cypress/Widget";
 
 export default class Widget extends BaseWidget {
+    static TimeoutType = TimeoutType;
     // Put custom actions here
 }
