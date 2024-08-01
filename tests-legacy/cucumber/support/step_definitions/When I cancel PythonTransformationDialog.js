8549be8904db56bbf18b2aa0f64fe61eefdc680a
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2024-08-01 14:11:41 +0300

    chore: remove tests-legacy

--- tests-legacy/cucumber/support/step_definitions/When I cancel PythonTransformationDialog.js
+++
@@ -1,7 +0,0 @@
-import { materialDesignerPage } from "../widgets/material_designer_page";
-
-export default function () {
-    this.When(/^I cancel PythonTransformationDialog$/, () => {
-        materialDesignerPage.designerWidget.pythonTransformationDialog.cancel();
-    });
-}
--- tests-legacy/cucumber/support/step_definitions/When I click the Run button.js
+++
@@ -1,7 +0,0 @@
-import { materialDesignerPage } from "../widgets/material_designer_page";
-
-export default function () {
-    this.When(/^I click the Run button$/, () => {
-        materialDesignerPage.designerWidget.pythonTransformationDialog.run();
-    });
-}
