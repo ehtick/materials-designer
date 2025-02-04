a320526c5e56f54eb0d0e279bc80324d69450782
VsevolodX <vsevolodbiryukov@icloud.com>
2025-02-04 12:17:53 -0800

    update: pass out dir

--- tests/generate-features.js
+++ tests/generate-features.js
@@ -1,5 +1,5 @@
 const { TestFeatureGenerator } = require("@mat3ra/tede/src/js/scripts/generate-features");
 
-const generator = new TestFeatureGenerator("./cypress/templates/");
+const generator = new TestFeatureGenerator("./cypress/templates/", "./cypress/e2e/");
 
 generator.generate();
